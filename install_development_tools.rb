59e3edb8033e088b2e437e57203e242c19b82231
Mário Marroquim <mariomarroquim@ufpi.edu.br>
2026-05-06 11:04:30 -0300

    Update development/production templates with Rails 8.1 changes

--- install_development_tools.rb
+++ install_development_tools.rb
@@ -2,65 +2,34 @@
 
 gem_group :development, :test do
   # Improve code debugging.
-  gem 'awesome_print'
+  gem "awesome_print"
 
   # Minimize database issues.
-  gem 'bullet'
+  gem "bullet"
 end
 
 gem_group :development do
   # Improve code debugging.
-  gem 'better_errors'
-  gem 'binding_of_caller'
-
-  # Secure code and libraries.
-  gem 'bundler-audit'
+  gem "better_errors"
+  gem "binding_of_caller"
 
   # Minimize database issues.
-  gem 'database_consistency', require: false
+  gem "database_consistency", require: false
 
   # Minimize performance issues.
-  gem 'rubocop-performance', require: false
+  gem "rubocop-performance", require: false
 end
 
 after_bundle do
-  run 'yes y | bundle exec rails g bullet:install'
+  run "yes y | bundle exec rails g bullet:install"
 
-  run 'bundle exec database_consistency install'
+  run "bundle exec database_consistency install"
 end
 
-inject_into_file '.rubocop.yml' do
+inject_into_file ".rubocop.yml" do
   "\n# Minimize performance issues.\nplugins: rubocop-performance\n"
 end
 
-file 'run_pre_commit_checks.sh', <<~CONTENT
-  #!/bin/bash
-
-  # Script for running various pre-commit checks for a Ruby on Rails project.
-
-  printf  "\\n== RUNNING BUNDLER-AUDIT ==\\n\\n"
-  bundle exec bundle-audit --update
-  sleep 3
-
-  printf  "\\n== RUNNING BRAKEMAN ==\\n"
-  bundle exec brakeman -A -q --summary
-  sleep 3
-
-  printf  "== RUNNING DATABASE-CONSISTENCY ==\\n\\n"
-  bundle exec database_consistency -f
-  sleep 3
-
-  printf  "\\n== RUNNING RUBOCOP ==\\n\\n"
-  bundle exec rubocop -a
-  sleep 3
-
-  printf  "\\n== RUNNING TESTS ==\\n\\n"
-  bundle exec rails test
-  sleep 3
-
-  printf  "\\n== RUNNING CLEANING ==\\n\\n"
-  bundle exec rails assets:clobber tmp:clear log:clear
-  printf "Done cleaning\\n"
-
-  printf  "\\n== ALL CHECKS DONE! ==\\n\\n"
-CONTENT
+inject_into_file "config/ci.rb", after: "  step \"Security: Brakeman code analysis\", \"bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error\"" do
+  "\n\n  # Run a database consistency check.\n  step \"Database: Database consistency check\", \"bundle exec database_consistency -f\"\n"
+end
--- install_production_tools.rb
+++ install_production_tools.rb
@@ -2,32 +2,30 @@
 
 gem_group :production do
   # Log requests in a single line.
-  gem 'lograge'
+  gem "lograge"
 end
 
 # Track changes to models.
-gem 'paper_trail'
+gem "paper_trail"
 
 after_bundle do
-  run 'bundle exec rails g paper_trail:install'
+  run "bundle exec rails g paper_trail:install"
 
-  run 'bundle exec rails db:migrate'
+  run "bundle exec rails db:migrate"
 end
 
-inject_into_file 'config/environments/production.rb',
-                 before: '# Settings specified here will take precedence over those in config/application.rb.' do
+inject_into_file "config/environments/production.rb", before: "# Settings specified here will take precedence over those in config/application.rb." do
   "# Log requests in a single line.\n  config.lograge.enabled = true\n  config.lograge.custom_payload do |controller|\n    { current_user_id: controller.current_user.try(:id) }\n  end\n\n  "
 end
 
-inject_into_file 'config/environments/test.rb', after: 'Rails.application.configure do' do
+inject_into_file "config/environments/test.rb", after: "Rails.application.configure do" do
   "\n  # Disable tracking changes to models.\n  PaperTrail.enabled = false\n"
 end
 
-inject_into_file 'app/models/application_record.rb', after: 'class ApplicationRecord < ActiveRecord::Base' do
+inject_into_file "app/models/application_record.rb", after: "class ApplicationRecord < ActiveRecord::Base" do
   "\n  # Track all changes to models.\n  has_paper_trail\n"
 end
 
-inject_into_file 'app/controllers/application_controller.rb',
-                 after: 'class ApplicationController < ActionController::Base' do
+inject_into_file "app/controllers/application_controller.rb", after: "class ApplicationController < ActionController::Base" do
   "\n  # Track who changed models.\n  before_action :set_paper_trail_whodunnit\n"
 end
