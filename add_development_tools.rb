d56e193d7da8b6959b7a38dd1cc06e8c531756da
Mário Marroquim <mariomarroquim@ufpi.edu.br>
2026-06-11 11:37:56 -0300

    Remove dependency on database_consistency Ruby gem

--- add_development_tools.rb
+++ add_development_tools.rb
@@ -13,23 +13,14 @@ gem_group :development do
   gem "better_errors"
   gem "binding_of_caller"
 
-  # Minimize database issues.
-  gem "database_consistency", require: false
-
   # Minimize performance issues.
   gem "rubocop-performance", require: false
 end
 
 after_bundle do
   run "yes y | bundle exec rails g bullet:install"
-
-  run "bundle exec database_consistency install"
 end
 
 inject_into_file ".rubocop.yml" do
   "\n# Minimize performance issues.\nplugins: rubocop-performance\n"
-end
-
-inject_into_file "config/ci.rb", after: "  step \"Security: Brakeman code analysis\", \"bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error\"" do
-  "\n\n  # Run a database consistency check.\n  step \"Database: Database consistency check\", \"bundle exec database_consistency -f\"\n"
 end
